41ec04941a7edb965fa10fd6cf4861ff0c3b57fb
nkadebug <nkadebug@gmail.com>
2021-01-01 16:55:58 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 let dt = new Date();
-let currDir = "dist/" + dt.toJSON().split("T")[0];
+let currDir = 'dist/' + dt.toJSON().split('T')[0];
 fs.mkdirSync(currDir, { recursive: true });
-fs.writeFileSync(currDir + "/index-" + dt.getTime() + ".json", JSON.stringify({ ts: new Date().toJSON() }, null, 4));
+fs.writeFileSync(
+  currDir + '/index-' + dt.getTime() + '.json',
+  JSON.stringify({ ts: new Date().toJSON(), n: Math.random() }, null, 4)
+);
