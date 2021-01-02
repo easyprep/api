1c33a7fcff1444ddd8ddc1815aee4f1a4ef35f18
nkadebug <nka.debug@gmail.com>
2021-01-02 04:03:10 +0000

    Update

---
+++ src/indexer.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+function indexDir(path) {
+    let indexData = {};
+    fs.readdirSync(path).forEach(function (file) {
+        let newPath = path + "/" + file;
+        let stats = fs.statSync(newPath);
+        if (stats.isDirectory()) {
+            indexData[file] = `${stats.mtimeMs}/dir`;
+            indexDir(newPath);
+        } else {
+            if (file.indexOf("index.") == -1) {
+                indexData[file] = `${stats.mtimeMs}`;
+            }
+        }
+    });
+    if (JSON.stringify(indexData) !== "{}") {
+        fs.writeFileSync(path + "/index.json", JSON.stringify(indexData));
+        console.log({ path, indexData });
+    }
+}
+
+indexDir("quizzes");
