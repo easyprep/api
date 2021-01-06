6bb0133b1bdb2107dfb0d35036e88520a634454d
nkadebug <nka.debug@gmail.com>
2021-01-06 06:04:49 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -3,88 +3,123 @@ const path = require('path');
 const fs = require('fs');
 const qs = require('qs');
 const axios = require('axios').default;
-const uuid = require('uuid').v5;
+
 const ts = new Date().getTime();
 
 let currDir = 'docs';
 let quesDir = path.join(currDir + '/questions');
 fs.mkdirSync(quesDir, { recursive: true });
 
-const url = process.env.api_url;
-const payload = JSON.parse(process.env.api_payload);
-const map = JSON.parse(process.env.data_map);
-const name_scape = process.env.my_name_scape;
+const apiUrl = process.env.api_url;
+const apiKey = process.env.api_key;
 
 let status = {
-  fetchCount: 1000,
-  lastIdFetched: 0,
+    startDate: "2017-01-01",
+    limit: 10,
 };
+
 let statusFilePath = path.join(currDir, '/status.json');
 
 try {
-  status = require('../' + statusFilePath);
+    status = require('../' + statusFilePath);
 } catch (e) {
-  console.log(e);
+    console.log(e);
 }
 
-let ids = [];
-let { lastIdFetched } = status;
-while (ids.length < status.fetchCount) {
-  ids.push(++lastIdFetched);
+let q = {
+    apiKey,
+    startDate: status.startDate,
+    startId: status.startId,
+    excludeFirst: true
 }
-payload.quesids = ids.join();
 
-axios.post(url, qs.stringify(payload)).then(function ({ data }) {
-  let baseUrl = url.split('/')[2];
-  let indexData = {};
-  if (!data.questions) {
-    console.log('New Questions: 0');
-    return;
-  }
-  console.log('New Questions: ' + data.questions.length);
-  data.questions.forEach(function (q) {
-    status.lastIdFetched = parseInt(q.id);
-    let nq = {
-      id: uuid(`${baseUrl}.${q.id}`, name_scape),
-    };
-    for (let key in map) {
-      nq[key] = q[map[key]] || '';
+axios.post(url, qs.stringify(q)).then(function ({ data }) {
+
+    console.log(data.data.length - 1);
+
+    if (data.err) {
+        console.error(data.msg);
+    } else {
+        let keys = data.data[0];
+        data.data.forEach((qArr, i) => {
+            if (i) {
+
+                let qJson = {};
+                qArr.forEach((a, j) => (qJson[keys[j]] = a));
+                delete qJson.source;
+
+                let qPath = path.join(quesDir, qJson.id);
+                fs.mkdirSync(qPath, { recursive: true });
+
+                fs.writeFileSync(
+                    path.join(qpath, "/index.json"),
+                    JSON.stringify(qJson, null, 2)
+                );
+
+                status.startDate = qJson.updated_at;
+                status.startId = qJson.id;
+
+            }
+        });
+
+        fs.writeFileSync('status.json', JSON.stringify(status, null, 4));
     }
-    nq.labels = ['Current Affairs', nq.created_at.split(' ')[0]];
-    let indexPath = nq.labels
-      .map((a) => a.toLowerCase().replace(/[^a-zA-Z0-9]+/g, '-'))
-      .join('/');
-    if (!indexData[indexPath]) indexData[indexPath] = {};
-    indexData[indexPath][nq.id] = new Date(nq.created_at).getTime();
-    fs.writeFileSync(
-      path.join(quesDir, nq.id + '.json'),
-      JSON.stringify(nq, null, 2)
-    );
-  });
-  status.fetchCount = data.questions.length < 100 ? 100 : 1000;
-  status.lastFetchCount = data.questions.length;
-  status.lastUpdatedAt = new Date().toJSON();
-  fs.writeFileSync(statusFilePath, JSON.stringify(status, null, 2));
 
-  let qIndex = [];
-  fs.readdirSync(quesDir).forEach(function (file) {
-    qIndex.push(file.split('.')[0]);
-  });
-  fs.writeFileSync(path.join(quesDir, '/index.json'), JSON.stringify(qIndex));
+    console.timeEnd();
 
-  for (let key in indexData) {
-    let indexFileDir = path.join(currDir, '/quizzes/', key);
-    let indexFilePath = path.join(indexFileDir + '/index.json');
-    let indexFileData = {};
-
-    try {
-      indexFileData = require('../' + indexFilePath);
-    } catch (e) {}
-
-    if (indexData[key]) {
-      indexFileData = { ...indexFileData, ...indexData[key] };
-      fs.mkdirSync(indexFileDir, { recursive: true });
-      fs.writeFileSync(indexFilePath, JSON.stringify(indexFileData, null, 2));
-    }
-  }
 });
+
+// let baseUrl = url.split('/')[2];
+// let indexData = {};
+// if (!data.questions) {
+//     console.log('New Questions: 0');
+//     return;
+// }
+// console.log('New Questions: ' + data.questions.length);
+
+// data.questions.forEach(function (q) {
+//     status.lastIdFetched = parseInt(q.id);
+//     let nq = {
+//         id: uuid(`${baseUrl}.${q.id}`, name_scape),
+//     };
+//     for (let key in map) {
+//         nq[key] = q[map[key]] || '';
+//     }
+//     nq.labels = ['Current Affairs', nq.created_at.split(' ')[0]];
+//     let indexPath = nq.labels
+//         .map((a) => a.toLowerCase().replace(/[^a-zA-Z0-9]+/g, '-'))
+//         .join('/');
+//     if (!indexData[indexPath]) indexData[indexPath] = {};
+//     indexData[indexPath][nq.id] = new Date(nq.created_at).getTime();
+//     fs.writeFileSync(
+//         path.join(quesDir, nq.id + '.json'),
+//         JSON.stringify(nq, null, 2)
+//     );
+// });
+// status.fetchCount = data.questions.length < 100 ? 100 : 1000;
+// status.lastFetchCount = data.questions.length;
+// status.lastUpdatedAt = new Date().toJSON();
+// fs.writeFileSync(statusFilePath, JSON.stringify(status, null, 2));
+
+// let qIndex = [];
+// fs.readdirSync(quesDir).forEach(function (file) {
+//     qIndex.push(file.split('.')[0]);
+// });
+// fs.writeFileSync(path.join(quesDir, '/index.json'), JSON.stringify(qIndex));
+
+// for (let key in indexData) {
+//     let indexFileDir = path.join(currDir, '/quizzes/', key);
+//     let indexFilePath = path.join(indexFileDir + '/index.json');
+//     let indexFileData = {};
+
+//     try {
+//         indexFileData = require('../' + indexFilePath);
+//     } catch (e) { }
+
+//     if (indexData[key]) {
+//         indexFileData = { ...indexFileData, ...indexData[key] };
+//         fs.mkdirSync(indexFileDir, { recursive: true });
+//         fs.writeFileSync(indexFilePath, JSON.stringify(indexFileData, null, 2));
+//     }
+// }
+// });
